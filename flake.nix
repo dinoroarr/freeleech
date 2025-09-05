2b1e61901cff833ffa8b21947eddcee99384dee9
dinoroarr <227888723+dinoroarr@users.noreply.github.com>
2025-09-05 20:21:05 +0000

    feat: Add tracker info

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "trackers freelech info";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachSystem [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = with pkgs; mkShell {
+          packages = [
+            pkl
+            nodejs
+          ];
+        };
+      }
+    );
+}
